ab77e23897d3d46e43cc9a7fc32f0d8449eb7661
Jonathan Toledo <kry0ster@gmail.com>
2021-07-13 21:26:50 +0000

    Releasing v1.7.2

--- core/banner.go
+++ core/banner.go
@@ -2,7 +2,7 @@ package core
 
 const (
 	Name       = "aquatone"
-	Version    = "1.7.1"
+	Version    = "1.7.2"
 	Author     = "Michael Henriksen"
 	Maintainer = "Jonathan Toledo"
 	Website    = "https://github.com/overjt/aquatone"
